60092511afce462a746608497972eb430635e711
cHELO <mgriveros@uc.cl>
2020-11-08 01:55:23 -0300

    fix install.sh script

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-pwd=$( aws ecr get-login-password )
+pwd=$( /usr/local/bin/aws ecr get-login-password )
 docker container stop $(docker container ls -aq)
 docker login -u AWS -p $pwd https://197979423491.dkr.ecr.us-east-2.amazonaws.com
 docker pull 197979423491.dkr.ecr.us-east-2.amazonaws.com/chat
