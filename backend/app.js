f0a3abf54115b2c7088e92fd94494a305d2e0212
cHELO <mgriveros@uc.cl>
2020-11-08 01:10:26 -0300

    test

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,12 @@ const cors = require("cors");
 
 // Setup server
 const app = express(); 
-const server = require("./config/express")(app);  
+const server = require("./config/express")(app);
+
+
+
+
+
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
